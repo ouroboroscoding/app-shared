956fb924ab34017079264a13b8aa65dd8c7d3f88
Chris Nasr <chris@ouroboroscoding.com>
2022-10-02 13:23:52 -0400

    development

--- navigator.js
+++ navigator.js
@@ -10,47 +10,40 @@
 
 // NPM imports
 import { clone } from '@ouroboros/tools';
-import { Platform } from "react-native";
-
-/**
- * Navigator
- *
- * Contains all details and
- */
 
 // Init the callback and screens lists
-const __callbacks = [];
-const __screens = [];
+const _callbacks = [];
+const _screens = [];
 
 // Set the initial limit of screens to keep in history
-const __limit = 10;
+const _limit = 10;
 
 /**
  * Back
  *
  * Goes back in the history
  *
- * @name _back
- * @access private
- * @returns Boolean
+ * @name back
+ * @access public
+ * @returns {boolean}
  */
-function _back() {
+export function back() {
 
 	// If we don't have any more screens
-	if(__screens.length <= 1) {
+	if(_screens.length <= 1) {
 		return false;
 	}
 
 	// Pull off the last screen
-	__screens.pop();
+	_screens.pop();
 
 	// Store the last index
-	let iLast = __screens.length - 1;
+	let iLast = _screens.length - 1;
 
 	// Go through callbacks and notify anyone who wants to know of screen
 	//	changes
-	for(let f of __callbacks) {
-		if(f(__screens[iLast]) === false) {
+	for(let f of _callbacks) {
+		if(f(_screens[iLast]) === false) {
 			return false;
 		}
 	}
@@ -60,19 +53,42 @@ function _back() {
 }
 
 /**
+ * Clear
+ *
+ * Removes all screens from the list, resetting the history
+ *
+ * @name clear
+ * @access public
+ * @return {void}
+ */
+export function clear() {
+
+	// Remove all screens
+	_screens = [];
+
+	// Go through callbacks and notify anyone who wants to know of screen
+	//	changes
+	for(let f of _callbacks) {
+		if(f(null) === false) {
+			return false;
+		}
+	}
+}
+
+/**
  * Current
  *
  * Returns the name and details of the current screen
  *
- * @name _current
- * @access private
- * @returns Object
+ * @name current
+ * @access public
+ * @returns {Object}
  */
-function _current() {
+export function current() {
 
 	// Return null if there's no current screen, else return the last screen set
-	return __screens.length === 0 ? null :
-			clone(__screens[__screens.length-1]);
+	return _screens.length === 0 ? null :
+			clone(_screens[_screens.length-1]);
 }
 
 /**
@@ -80,29 +96,24 @@ function _current() {
  *
  * Sets or gets the current limit of history screens to keep
  *
- * @name _limit
- * @access private
- * @param {Number} count An unsigned number to indicate the maximum history
- * @returns Number
+ * @name limit
+ * @access public
+ * @param {number} count An unsigned number to indicate the maximum history
+ * @returns {number}
  */
-function _limit(count=null) {
-
-	// Do nothing on IOS
-	if(Platform.OS === 'ios') {
-		return false;
-	}
+export function limit(count=null) {
 
 	// If we got a count, set it
 	if(count) {
 		if(count < 2) {
 			throw new Error('Navigator history limit must be at least 1, otherwise, what\'s the point?');
 		}
-		__limit = count;
+		_limit = count;
 	}
 
 	// Else, return the current limit
 	else {
-		return __limit;
+		return _limit;
 	}
 }
 
@@ -111,34 +122,30 @@ function _limit(count=null) {
  *
  * Set the screen to be displayed
  *
- * @name _set
- * @access private
- * @param {String} name The name of the screen to set
+ * @name set
+ * @access public
+ * @param {string} name The name of the screen to set
  * @param {Object} details The additional props to pass to the screen
- * @returns void
+ * @returns {void}
  */
-function _set(name, details={}) {
+export function set(name, details={}) {
 
-	// If we're on android
-	if(Platform.OS === 'android') {
+	// If the current history count is at max
+	if(_screens.length === _limit) {
 
-		// If the current history count is at max
-		if(__screens.length === __limit) {
-
-			// Cut out the oldest
-			__screens.splice(0,1);
-		}
-
-		// Add the new screen data to the list
-		__screens.push({
-			name: name,
-			details: details
-		});
+		// Cut out the oldest
+		_screens.splice(0,1);
 	}
+
+	// Add the new screen data to the list
+	_screens.push({
+		name: name,
+		details: details
+	});
 
 	// Go through callbacks and notify anyone who wants to know of screen
 	//	changes
-	for(let f of __callbacks) {
+	for(let f of _callbacks) {
 		f({name: name, details: details});
 	}
 }
@@ -148,15 +155,15 @@ function _set(name, details={}) {
  *
  * Called to store a callback when the screen changes
  *
- * @name _subscribe
- * @access private
- * @param {Function} callback The function to call on screen changes
- * @returns void
+ * @name subscribe
+ * @access public
+ * @param {export function} callback The export function to call on screen changes
+ * @returns {void}
  */
-function _subscribe(callback) {
+export function subscribe(callback) {
 
 	// Add the callback to the list
-	__callbacks.push(callback);
+	_callbacks.push(callback);
 }
 
 /**
@@ -164,19 +171,19 @@ function _subscribe(callback) {
  *
  * Called to remove a callback from being called when the screen changes
  *
- * @name _unsubscribe
- * @access private
- * @param {Function} callback The function to remove
- * @returns Boolean
+ * @name unsubscribe
+ * @access public
+ * @param {export function} callback The export function to remove
+ * @returns {boolean}
  */
-function _unsubscribe(callback) {
+export function unsubscribe(callback) {
 
 	// Find the index of the callback
-	let iIndex = __callbacks.indexOf(callback);
+	let iIndex = _callbacks.indexOf(callback);
 
 	// If it exists, remove it
 	if(iIndex > -1) {
-		__callbacks.splice(iIndex, 1);
+		_callbacks.splice(iIndex, 1);
 		return true;
 	} else {
 		return false;
@@ -185,11 +192,6 @@ function _unsubscribe(callback) {
 
 // Default export
 const navigator = {
-	back: _back,
-	current: _current,
-	limit: _limit,
-	set: _set,
-	subscribe: _subscribe,
-	unsubscribe: _unsubscribe
+	back, current, limit, set, subscribe, unsubscribe
 };
 export default navigator;
